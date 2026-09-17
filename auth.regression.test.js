820533976bce5665e1a70f9be7ba726a63326f1e
qthaizzz <quocthaideptrai4264@gmail.com>
2026-09-17 19:47:31 +0700

    Update auth.regression.test.js

--- auth.regression.test.js
+++ auth.regression.test.js
@@ -1,1 +1,37 @@
+const login = require("./auth");
 
+describe("Regression Test - Login", () => {
+
+    test("Sai mật khẩu", () => {
+        expect(login("admin", "wrong")).toBe(false);
+    });
+
+    test("Username rỗng", () => {
+        expect(login("", "123")).toBe(false);
+    });
+
+    test("Password rỗng", () => {
+        expect(login("admin", "")).toBe(false);
+    });
+
+    test("Username không tồn tại", () => {
+        expect(login("user", "123")).toBe(false);
+    });
+
+    test("Password chứa ký tự đặc biệt", () => {
+        expect(login("admin", "@#$%^")).toBe(false);
+    });
+
+    test("Username chứa ký tự đặc biệt", () => {
+        expect(login("@dmin", "123")).toBe(false);
+    });
+
+    test("Tài khoản bị khóa", () => {
+        expect(login("locked", "123")).toBe(false);
+    });
+
+    test("Sai cả username và password", () => {
+        expect(login("user", "wrong")).toBe(false);
+    });
+
+});
