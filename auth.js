afd746dc3ba760e45f5caece8a5c42ab095a9ae4
qthaizzz <quocthaideptrai4264@gmail.com>
2026-09-17 19:46:42 +0700

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,1 +1,15 @@
+function login(username, password) {
+    // Tài khoản bị khóa
+    if (username === "locked") {
+        return false;
+    }
 
+    // Đăng nhập hợp lệ
+    if (username === "admin" && password === "123") {
+        return true;
+    }
+
+    return false;
+}
+
+module.exports = login;
